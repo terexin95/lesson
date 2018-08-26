f62c17586f788e49ce1635c896a22bf45a9ae171
Vadim <alex.alex.158134@gmail.com>
2018-08-26 15:32:58 +0300

    commit-9

--- lesson 8/js/script.js
+++ lesson 8/js/script.js
@@ -32,66 +32,62 @@ window.addEventListener('DOMContentLoaded', function(){
 			}
 		}
 	});
+
+	let deadline = '2018-08-26';
+
+
+	function timeR(endTime) {
+		let t = Date.parse(endTime) - Date.parse(new Date()),
+		seconds = Math.floor((t/1000) % 60),
+		min = Math.floor((t/1000/60) % 60),
+		hours = Math.floor(t/(1000*60*60));
+		console.log(t.hours);
+		if (seconds < 10) {
+			seconds = '0' + seconds
+		}
+
+		if (min < 10) {
+			seconds = '0' + seconds
+		}
+
+		if (hours < 10) {
+			seconds = '0' + seconds
+		}
+		return {
+			total: t,
+			hours: hours,
+			min: min,
+			seconds: seconds
+		}
+
+	};
+
+	function clockTime(id, endTime){
+		let timer = document.getElementById(id),
+			hours = timer.querySelector('.hours'),
+			min = timer.querySelector('.minutes'),
+			seconds = timer.querySelector('.seconds');
+
+			function timeIntr() {
+				let t = timeR(endTime);
+				hours.innerHTML = t.hours;
+				min.innerHTML = t.min;
+				seconds.innerHTML = t.seconds;
+				if (t.total <= 0 ) {
+					clearInterval(timeIntr);
+					hours.innerHTML = '00';
+					min.innerHTML = '00';
+					seconds.innerHTML = '00';
+				}
+			};
+			timeIntr();
+
+			timeInterval = setInterval(timeIntr, 1000);
+	};
+
+	clockTime('timer', deadline);
+
 });
 
 
-let deadline = '2018-08-26';
-
-
-function timeR(endTime) {
-	let t = Date.parse(endTime) - Date.parse(new Date()),
-	seconds = Math.floor((t/1000) % 60),
-	min = Math.floor((t/1000/60) % 60),
-	hours = Math.floor(t/(1000*60*60));
-	console.log(t.hours);
-	if (seconds < 10) {
-		seconds = '0' + seconds
-	}
-
-	if (min < 10) {
-		seconds = '0' + seconds
-	}
-
-	if (hours < 10) {
-		seconds = '0' + seconds
-	}
-
-	if (hours <= 0) {
-		timer.style.display = 'none';
-	}
-	return {
-		total: t,
-		hours: hours,
-		min: min,
-		seconds: seconds
-	}
-
-};
-
-function clockTime(id, endTime){
-	let timer = document.getElementById(id),
-		hours = timer.querySelector('.hours'),
-		min = timer.querySelector('.minutes'),
-		seconds = timer.querySelector('.seconds');
-
-		function timeIntr() {
-			let t = timeR(endTime);
-			hours.innerHTML = t.hours;
-			min.innerHTML = t.min;
-			seconds.innerHTML = t.seconds;
-			if (t.total <= 0 ) {
-				clearInterval(timeInterval);
-			}
-		};
-		timeIntr();
-
-		timeInterval = setInterval(timeIntr, 1000);
-
-		
-
-
-
-};
-
-clockTime('timer', deadline);
 
