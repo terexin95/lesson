d0dc956d9f1bcee646c09ae1a24e18bc79d9f645
Vadim <alex.alex.158134@gmail.com>
2018-09-02 16:31:04 +0300

    commit-12

---
+++ lesson 15/test/text.js
@@ -0,0 +1,9 @@
+
+
+let assert = require('chai').assert
+
+describe('sum', function() {
+	it('Тут условие задачи на свое усмотрение, то есть что ты этой функцией хочешь узнать', function() {
+	         assert.equal(sum(2, 2), true);
+	    });
+});
